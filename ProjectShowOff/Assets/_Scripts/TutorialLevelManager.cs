38fa27037d37049c7d7977cd3463c89a60d90d9f
Steefman <svolkers99@gmail.com>
2020-06-03 15:38:37 +0200

    Implementated powerup part in the tutorial

--- ProjectShowOff/Assets/_Scripts/TutorialLevelManager.cs
+++ ProjectShowOff/Assets/_Scripts/TutorialLevelManager.cs
@@ -2,27 +2,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TutorialLevelManager : MonoBehaviour
 {
     public enum state { start, movement, powerupCollect, powerUpActivate, done }
     public state _tutorialState = state.start;
 
-    [Title("Level State 1")]
+    [Title("Level State 1 (Start)")]
     [SerializeField] private GameObject _uiPopup = null;
     [SerializeField] private Rigidbody _playerBody = null;
     [SerializeField] private ThrashSpawner _thrashSpawner = null;
     [SerializeField] private Vector3 _playerStartForce = new Vector3();
     [SerializeField] private List<Fish> _fishes = new List<Fish>();
 
-    [Title("Level State 2")]
+    [Title("Level State 2(Movement)")]
     [SerializeField] private PlayerMovement _playerMovement = null;
     [SerializeField] private GameObject _movementExplenation = null;
     [SerializeField] private Thrash _firstThrash = null;
 
-    [Title("Level State 3")]
+    [Title("Level State 3 (Power up collection)")]
     [SerializeField] private GameObject _bottle1 = null;
     [SerializeField] private GameObject _ring2 = null;
+    [SerializeField] private GameObject _animalExplenation = null;
+    [SerializeField] private float _freezeAfterSeconds = 5;
+    [SerializeField] private GameObject _powerupExplenation = null;
+    private bool _playerLanded = false;
+
+    [Title("Level State 4 (Power up activation)")]
+    [SerializeField] private Button _powerupActivationButton = null;
+    [SerializeField] private GameObject _bottle2 = null;
+    [SerializeField] private GameObject _tutorialEndPopup = null;
 
     private Vector3 playerStartPos = Vector3.zero;
 
@@ -36,10 +46,10 @@ public class TutorialLevelManager : MonoBehaviour
 
     void Update()
     {
-        switch(_tutorialState)
+        switch (_tutorialState)
         {
             case state.start:
-                foreach(Fish fish in _fishes)
+                foreach (Fish fish in _fishes)
                 {
                     if (fish.health < 100)
                     {
@@ -58,27 +68,44 @@ public class TutorialLevelManager : MonoBehaviour
                 if (_firstThrash == null) _tutorialState = state.movement;
                 break;
             case state.movement:
-                if(_firstThrash.health <= 0 || _firstThrash == null)
+                if (_firstThrash.health <= 0 || _firstThrash == null)
                 {
                     _movementExplenation.SetActive(false);
                     _uiPopup.SetActive(true);
                     _bottle1.SetActive(true);
                     _ring2.SetActive(true);
+
+                    StartCoroutine(showAnimalExplenation(5));
+                    StartCoroutine(delayedFreeze(_freezeAfterSeconds));
+                    _playerMovement.startJump += startJump;
+
+
                     _tutorialState = state.powerupCollect;
                 }
                 break;
             case state.powerupCollect:
-                if(_bottle1 == null && _ring2 == null)
+                if (_bottle1 == null && _ring2 == null)
                 {
-                   // _tutorialState = state.powerUpActivate;
-                    _thrashSpawner.StartSpawning();
-                    _tutorialState = state.done;
+                    _playerMovement.endJump += OnPlayerLanded;
+
+                    if (_playerLanded)
+                    {
+                        Time.timeScale = 0;
+                        _powerupExplenation.SetActive(true);
+                        _playerMovement.endJump += OnPowerup;
+                        _powerupActivationButton.onClick.AddListener(powerUpButtonPressed);
+                        _tutorialState = state.powerUpActivate;
+                    }
                 }
                 break;
             case state.powerUpActivate:
-                // if powerup activated:
-                //_thrashSpawner.StartSpawning();
-                // 
+                if (_bottle2 == null)
+                {
+                    StartCoroutine(showFinalPopup(5));
+                    _thrashSpawner.StartSpawning();
+
+                    _tutorialState = state.done;
+                }
                 break;
             case state.done:
                 break;
@@ -86,17 +113,24 @@ public class TutorialLevelManager : MonoBehaviour
     }
 
 
-    private IEnumerator delayedFreeze(float pTime)
+    private IEnumerator delayedFreeze(float pDuration)
     {
-        yield return new WaitForSeconds(pTime);
-        Time.timeScale = 0;
+        float timer = 0;
+        while((_bottle1 == null && _ring2 == null) == false)
+        {
+            timer += Time.deltaTime;
+            if (timer > pDuration) break;
+            yield return null;
+        }
+
+        if(timer > pDuration) Time.timeScale = 0;
     }
 
 
     private void startJump(object pSender, Vector3 pPosition)
     {
+        if(_bottle1 == null && _ring2 == null) _playerMovement.startJump -= startJump;
         Time.timeScale = 1;
-        _playerMovement.startJump -= startJump;
     }
 
     private void SetStartPos(object pSender, Vector3 pPosition)
@@ -109,5 +143,41 @@ public class TutorialLevelManager : MonoBehaviour
     private void StartTutorial()
     {
         _playerBody.AddForce(_playerStartForce, ForceMode.Impulse);
+    }
+
+
+    private IEnumerator showAnimalExplenation(float pDuration)
+    {
+        _animalExplenation.SetActive(true);
+        yield return new WaitForSeconds(pDuration);
+        _animalExplenation.SetActive(false);
+    }
+
+
+    private void OnPlayerLanded(object pSender, Vector3 pPosition)
+    {
+        _playerLanded = true;
+    }
+
+
+    private void powerUpButtonPressed()
+    {
+        Time.timeScale = 1;
+        _bottle2.SetActive(true);
+        _powerupActivationButton.onClick.RemoveListener(powerUpButtonPressed);
+    }
+
+
+    private void OnPowerup(object pSender, Vector3 pPosition)
+    {
+        _powerupExplenation.SetActive(false);
+    }
+
+
+    private IEnumerator showFinalPopup(float pDuration)
+    {
+        _tutorialEndPopup.SetActive(true);
+        yield return new WaitForSeconds(pDuration);
+        _tutorialEndPopup.SetActive(false);
     }
 }
