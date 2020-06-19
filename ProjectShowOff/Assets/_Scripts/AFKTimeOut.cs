65da6ad755f8893990137b082a175741f53c4302
Steefman <svolkers99@gmail.com>
2020-06-19 11:47:08 +0200

    Created Inactivity time-out

---
+++ ProjectShowOff/Assets/_Scripts/AFKTimeOut.cs
@@ -0,0 +1,29 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AFKTimeOut : MonoBehaviour
+{
+    [SerializeField] private float _resetGameAfterSeconds = 30;
+    [SerializeField] private string _loadSceneWhenInactive = "MainScreen";
+
+    [SerializeField, ReadOnly] private float _inactiveTimer = 0;
+    private bool _inactive = false;
+
+
+    void Update()
+    {
+        _inactiveTimer += Time.deltaTime;
+
+        if(Input.GetMouseButton(0))
+        {
+            _inactiveTimer = 0;
+        }
+
+        if(_inactiveTimer > _resetGameAfterSeconds && !_inactive)
+        {
+            _inactive = true;
+            SceneManager.LoadSceneAsync(_loadSceneWhenInactive);
+        }
+    }
+}
