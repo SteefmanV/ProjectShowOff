961c02dd721cb75b32f4490ec1657104f7587cf5
Steefman <svolkers99@gmail.com>
2020-06-22 09:29:37 +0200

    Paused trash falling animation when colliding with environment. Improved coral bounce animation

---
+++ ProjectShowOff/Assets/_Scripts/CoralBounce.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoralBounce : MonoBehaviour
+{
+    [SerializeField] private float _timeBetweenBounces = 0;
+    private Animator _anim;
+
+    private float _timer = 0;
+
+    private void Start()
+    {
+        _anim = GetComponent<Animator>();
+    }
+
+
+    private void Update()
+    {
+        _timer += Time.deltaTime;
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            if (_timer > _timeBetweenBounces)
+            {
+                _anim.SetTrigger("bounce");
+                _timer = 0;
+            }
+        }
+    }
+}
---
+++ ProjectShowOff/Assets/_Scripts/TrashPauseAnimation.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrashPauseAnimation : MonoBehaviour
+{
+    [SerializeField] private float _keepPausingSeconds = 0.5f;
+    private Animator _anim;
+    private float _timer = 0;
+
+    private bool _paused = true;
+
+    private void Start()
+    {
+        _anim = GetComponent<Animator>();
+    }
+
+
+    private void Update()
+    {
+        _timer += Time.deltaTime;
+
+        if (_paused && _timer > _keepPausingSeconds)
+        {
+            _anim.SetBool("pause", false);
+            _paused = false;
+        }
+
+        if (!_paused && _timer < _keepPausingSeconds)
+        {
+            _anim.SetBool("pause", true);
+            _paused = true;
+        }
+    }
+
+
+    private void OnCollisionStay(Collision collision)
+    {
+        _timer = 0;
+    }
+}
